982baf66d3f1516c69741a9a14a0947aeb59a7e8
liamiracle1006 <miraclelia1006@gmail.com>
2026-02-03 22:39:00 +0800

    update: reshape the IP address

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://13.214.215.241:8090',
+        target: 'http://47.129.124.55:8090',
         changeOrigin: true,
         secure: false,
       },
